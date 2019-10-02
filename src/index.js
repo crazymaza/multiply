e01fbf5d2a1335a24b2618ce5d37340979b235a7
crazymaza <dx2389@gmail.com>
2019-10-02 20:02:11 +0300

    init: start Multiply task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+    let aArray = first.split('').reverse();
+    let bArray = second.split('').reverse();
+    let result = [];
+
+    for (let i = 0; i < aArray.length; i++) {
+        for (let j = 0; j < bArray.length; j++) {
+            if (!result[i + j]) {
+                result[i + j] = 0;
+            }
+            result[i + j] += aArray[i] * bArray[j];
+        }
+    }
+
+    for (let i = 0; i < result.length; i++) {
+        if (result[i] >= 10) {
+            if(!result[i + 1]) {
+                result[i + 1] = 0;
+            }
+            result[i + 1] += parseInt(result[i] / 10);
+            result[i] %= 10;
+        } 
+    }
+    return result.reverse().join('');
+};
